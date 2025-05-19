650e7fa9c4120ae60ffa5f1ee571c3fdfc6f8123
zxy <46674730+CUHKSZzxy@users.noreply.github.com>
2025-05-19 16:55:23 +0800

    Create launch_singlenode_tp.sh

---
+++ launch_singlenode_tp.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=0,1 lmdeploy serve api_server \
+        models--Qwen--Qwen2.5-7B-Instruct \
+        --tp 2 \
+        --server-port 23333 \
+        --backend pytorch \
+        --log-level INFO 2>&1 | tee 0_serve.log
