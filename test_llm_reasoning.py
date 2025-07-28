1570fbb891c0519b510e562bee47c2aceb9c3a57
zxy <46674730+CUHKSZzxy@users.noreply.github.com>
2025-07-28 18:11:24 +0800

    Create test_llm_reasoning.py

---
+++ test_llm_reasoning.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+client = OpenAI(api_key='YOUR_API_KEY', base_url='http://0.0.0.0:8001/v1')
+model_name = client.models.list().data[0].id
+
+response = client.chat.completions.create(
+    model=model_name,
+    messages=[{
+        'role':
+        'user',
+        'content': [{
+            'type': 'text',
+            'text': 'Who are you?',
+        }],
+    }],
+    temperature=0.8,
+    top_p=0.8,
+    # extra_body for reasoning
+    extra_body={
+        "chat_template_kwargs": {"enable_thinking": False}
+    }
+)
+print(response)
